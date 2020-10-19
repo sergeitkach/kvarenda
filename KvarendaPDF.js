2321c801cd05002baa0f39c0b4e79f945c1602ba
Sergey Tkach <57631348+sergeitkach@users.noreply.github.com>
2020-10-19 18:52:30 +0300

    Add files via upload

---
+++ KvarendaPDF.js
@@ -0,0 +1,47 @@
+function getCustomerTablePDF(header='', subheader='', text='', tableData){
+
+	let docDefinition = {
+		content: [
+			{text: header, style: 'header'},
+			{text: subheader, style: 'subheader'},
+			text,
+			{
+				style: 'table',
+				table: {
+					body: tableData
+				},
+				layout: {
+					fillColor: function (rowIndex, node, columnIndex) {
+						return (rowIndex % 2 === 0 && rowIndex != 0) ? '#CCCCCC' : null;
+					}
+				}
+			}
+		],
+		styles: {
+			header: {
+				fontSize: 18,
+				bold: true,
+				margin: [0, 0, 0, 10]
+			},
+			subheader: {
+				fontSize: 16,
+				bold: true,
+				margin: [0, 10, 0, 5]
+			},
+			table: {
+				margin: [0, 15, 0, 15],
+				fontSize: 9,
+				alignment: 'right'
+			},
+			tableHeader: {
+				bold: true,
+				fontSize: 10,
+				color: 'black',
+				alignment: 'center'
+			}
+		}
+	}
+
+	pdfMake.createPdf(docDefinition).download('Kvarenda');
+
+}
